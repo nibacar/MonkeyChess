4b029fbc24f89d08dc9a1b8963d5a430b0c8e327
nibacar <81136535+nibacar@users.noreply.github.com>
2023-05-16 14:22:14 -0700

    Add files via upload

---
+++ Drivercode.py
@@ -0,0 +1,8 @@
+import time, sys
+import RPi. GPIO as GPIO
+
+GPIO.setmode (GPIO.BOARD)
+GPIO.setup(15, GPIO .IN, pull_up_down = GPIO.PUD_UP)
+while True:
+    input = GPIO.input(15)
+    print (input)
