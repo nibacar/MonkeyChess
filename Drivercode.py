bf639cfe2b76a28c58c71efae1918669137c8b2f
nibacar <81136535+nibacar@users.noreply.github.com>
2023-05-17 11:20:11 -0700

    Update Drivercode.py

--- Drivercode.py
+++ Drivercode.py
@@ -5,8 +5,8 @@ GPIO.setmode (GPIO.BOARD)
 GPIO.setup(15, GPIO .IN, pull_up_down = GPIO.PUD_UP)
 while True:
     input = GPIO.input(15)
-    if input == 1:
-        print("NO MAGNET")
-    else: 
-        print("OMG A MAGNET")
+if input == 1:
+    print("NO MAGNET")
+else: 
+    print("OMG A MAGNET")
      
