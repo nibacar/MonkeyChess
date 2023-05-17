387e12ecc14753f419a5077e8dcf070b89d526a4
nibacar <81136535+nibacar@users.noreply.github.com>
2023-05-17 11:28:16 -0700

    Update Drivercode.py

--- Drivercode.py
+++ Drivercode.py
@@ -4,7 +4,7 @@ import RPi. GPIO as GPIO
 GPIO.setmode (GPIO.BOARD)
 GPIO.setup(15, GPIO .IN, pull_up_down = GPIO.PUD_UP)
 while True:
-    if GPIO.input == 1:
+    if GPIO.input == 0:
         print("NO MAGNET")
     else: 
         print("OMG A MAGNET")
