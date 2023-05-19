51ddba590b790eb86580dc49eda7447bc20b6619
nibacar <81136535+nibacar@users.noreply.github.com>
2023-05-19 10:29:29 -0700

    Update Drivercode.py

--- Drivercode.py
+++ Drivercode.py
@@ -6,7 +6,11 @@ GPIO.setup(18, GPIO.OUT)
 GPIO.setup(16, GPIO.OUT)
 GPIO.setup(15, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 GPIO.setup(13, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-while True:
-    arrayrows = [[GPIO.output(15),GPIO.output(13)]]
-    arraycolumns =[[GPIO.input(18),GPIO.input(16)]]
-    print("Magnet is on row:" + arrayrows.index(0)+1 + "\n and column: " + arraycolumns.index(0)+1)
+try:
+    while True:
+        arrayrows = [[GPIO.output(15, GPIO.LOW),GPIO.output(13, GPIO.LOW)]]
+        arraycolumns =[[GPIO.input(18),GPIO.input(16)]]
+        print("Magnet is on row:" + arrayrows.index(0)+1 + "\n and column: " + arraycolumns.index(0)+1)
+        
+except KeyboardInterrupt:
+    GPIO.cleanup()
