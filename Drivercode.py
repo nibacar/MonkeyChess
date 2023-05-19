ed37a7c14bcfc13e4f04cddf84036b82c17c8cd6
nibacar <81136535+nibacar@users.noreply.github.com>
2023-05-19 14:17:10 -0700

    Update Drivercode.py

--- Drivercode.py
+++ Drivercode.py
@@ -7,15 +7,13 @@ GPIO.setwarnings(False)
 GPIO.setmode (GPIO.BOARD)
 GPIO.setup(outputs, GPIO.OUT)
 GPIO.setup(inputs, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-arrayrows = [[GPIO.output(15, GPIO.LOW),GPIO.output(13, GPIO.LOW)]]
-arraycolumns =[[GPIO.input(18),GPIO.input(16)]]
 try:
     while True:
         for i in outputs:
             GPIO.output(i, GPIO.HIGH)
             for j in inputs:
                 if GPIO.input(j) == 0:
-                    print("Magnet is on row:" + str(i) + " and column: " + str(j))
+                    print("Magnet is on row:" + str(outputs.index(i)) + " and column: " + str(inputs.index(j)))
             GPIO.output(outputs, GPIO.LOW)
 
 except KeyboardInterrupt:
