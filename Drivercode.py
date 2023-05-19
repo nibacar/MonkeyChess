8bbdd82dce576a41d85f7eb8a7ab1eb98c3a8122
nibacar <81136535+nibacar@users.noreply.github.com>
2023-05-19 10:15:53 -0700

    Update Drivercode.py

--- Drivercode.py
+++ Drivercode.py
@@ -2,10 +2,11 @@ import time, sys
 import RPi. GPIO as GPIO
 
 GPIO.setmode (GPIO.BOARD)
-GPIO.setup(15, GPIO .IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(18, GPIO.OUT)
+GPIO.setup(16, GPIO.OUT)
+GPIO.setup(15, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(13, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 while True:
-    if GPIO.input(15) == 1:
-        print("NO MAGNET")
-    else: 
-        print("OMG A MAGNET")
-     
+    arrayrows = [[GPIO.output(15),GPIO.output(13)]]
+    arraycolumns =[[GPIO.input(18),GPIO.input(16)]]
+    print("Magnet is on row:" + arrayrows.index(0)+1 + "\n and column: " + arraycolumns.index(0)+1)
