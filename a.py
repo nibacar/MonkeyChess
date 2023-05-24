13c577682790f09ad6e88ae5337554d45fabbe35
nibacar <81136535+nibacar@users.noreply.github.com>
2023-05-24 14:12:23 -0700

    Update a.py

---
+++ a.py
@@ -0,0 +1,55 @@
+import RPi.GPIO as GPIO
+import time
+
+# Define the GPIO pins for rows and columns
+row_pins = [2, 3, 4, 17]  # Example row GPIO pins
+col_pins = [11, 5, 6, 13]  # Example column GPIO pins
+
+# Set up GPIO mode
+GPIO.setwarnings(False)
+GPIO.setmode (GPIO.BOARD)
+
+# Set up GPIO pins for rows as inputs with pull-up resistors
+for row_pin in row_pins:
+    GPIO.setup(row_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+# Set up GPIO pins for columns as outputs
+for col_pin in col_pins:
+    GPIO.setup(col_pin, GPIO.OUT)
+
+def detect_magnets():
+    magnets = []
+    for col_pin in col_pins:
+        # Set the current column pin to LOW
+        GPIO.output(col_pin, GPIO.LOW)
+
+        for row_pin in row_pins:
+            # Read the state of the current row pin
+            state = GPIO.input(row_pin)
+
+            # Check if a magnet is detected
+            if state == GPIO.LOW:
+                # Magnet detected, get the row and column
+                row = row_pins.index(row_pin)
+                col = col_pins.index(col_pin)
+                magnets.append((row, col))
+
+        # Reset the current column pin to HIGH
+        GPIO.output(col_pin, GPIO.HIGH)
+
+    return magnets
+
+try:
+    while True:
+        input("Press Enter to detect magnets...")
+        magnets = detect_magnets()
+        if magnets:
+            print("Magnets detected at:")
+            for magnet in magnets:
+                print(f"Row: {magnet[0]}, Column: {magnet[1]}")
+        else:
+            print("No magnets detected.")
+
+except KeyboardInterrupt:
+    # Clean up GPIO on keyboard interrupt
+    GPIO.cleanup()
