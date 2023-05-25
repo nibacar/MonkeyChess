db86ad0d332e31ee82570ae9ef58432e4f1c431f
nibacar <81136535+nibacar@users.noreply.github.com>
2023-05-25 10:22:48 -0700

    Update a.py

--- a.py
+++ a.py
@@ -3,21 +3,25 @@ import time
 
 # Define the GPIO pins for rows and columns
 row_pins = [16, 15, 13, 11]  # Example row GPIO pins
-col_pins = [36, 31, 29, 18]  # Example column GPIO pins
+col_pins = [36, 31, 29, 18]  # Example column GPIO pins\
 
 # Set up GPIO mode
-GPIO.setwarnings(False)
-GPIO.setmode (GPIO.BOARD)
+GPIO.setmode(GPIO.BOARD)
 
 # Set up GPIO pins for rows as inputs with pull-up resistors
-GPIO.setup(row_pins, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(col_pins, GPIO.OUT)
+for row_pin in row_pins:
+    GPIO.setup(row_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+# Set up GPIO pins for columns as outputs
+for col_pin in col_pins:
+    GPIO.setup(col_pin, GPIO.OUT)
 
 def detect_magnets():
     magnets = []
     for col_pin in col_pins:
         # Set the current column pin to LOW
         GPIO.output(col_pin, GPIO.LOW)
+        time.sleep(0.01)  # Adjust the delay here if needed
 
         for row_pin in row_pins:
             # Read the state of the current row pin
@@ -42,7 +46,7 @@ try:
         if magnets:
             print("Magnets detected at:")
             for magnet in magnets:
-                print(f"Row: {magnet[0]+1}, Column: {magnet[1]+1}")
+                print(f"Row: {magnet[0]}, Column: {magnet[1]}")
         else:
             print("No magnets detected.")
 
